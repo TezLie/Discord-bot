cbac39f9b176c3208094a1f9dcedd2a7ee44ac56
TezLie <hennrikgulbrandsen@gmail.com>
2018-09-25 17:43:19 +0200

    Working

--- EXPERIMENTAL/commands/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'server',
-    description: 'Displays info about the server',
-    execute(message, args) {
-        message.channel.send(`This server's name is: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    },
-};
--- EXPERIMENTAL/commands/userinfo.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'userinfo',
-    description: 'Displays userinfo',
-    execute(message, args) {
-        message.channel.send(`Your username: ${message.author.username}\nYour id: ${message.author.id}`);
-    },
-};
