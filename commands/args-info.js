76ad03e56416ca2fa25de2c7d8f9968e5a5d450b
TezLie <hennrikgulbrandsen@gmail.com>
2019-02-13 12:11:10 +0100

    fixed tabs

--- commands/args-info.js
+++ commands/args-info.js
@@ -2,10 +2,10 @@ module.exports = {
     name: 'args-info',
 	description: 'arguments',
 	guildOnly: false,
-	args: true,
-	usage: '<argument> {argument}...',
+  	args: true,
+  	usage: '<argument> {argument}...',
     execute(message, args) {
-		if (args[0] === 'foo') {
+        if (args[0] === 'foo') {
 			return message.channel.send('bar');
 		}
 		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
